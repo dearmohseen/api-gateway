7255d31c9376b57fb46d2083262b42e2795af34e
dearmohseen <mohseenkhan@Mohseens-MacBook-Pro.local>
2025-01-03 10:51:56 -0800

    initial changes

---
+++ src/main/java/com/mk/gateway/filter/RequestFilter.java
@@ -0,0 +1,16 @@
+package com.mk.gateway.filter;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+public class RequestFilter implements GatewayFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
